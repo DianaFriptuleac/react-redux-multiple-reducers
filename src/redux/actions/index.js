03b6abe8df033a1d92221993f224893ed4699ab7
Diana <dianadorojuc3@gmail.com>
2024-09-03 14:52:28 +0200

    fetch commit

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,6 @@
 export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES'
 export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES'
+export const GET_COMPANY = 'GET_COMPANY'
 
 export const addToFavourites = (company) =>{
     return {
@@ -12,5 +13,27 @@ export const removeFromFavourites = (company) =>{
     return {
         type: REMOVE_FROM_FAVOURITES,
         payload:company,
+    }
+}
+
+export const getSearchAction = () =>{
+    return(dispatch, getState) =>{
+        fetch('https://strive-benchmark.herokuapp.com/api/jobs?search=')
+        .then((response) =>{
+            if(response.ok) {
+                return response.json()
+            }else {
+                throw new Error('errore nel recupero dati')
+            }
+        })
+        .then((arrayOfCompany) =>{
+            dispatch({
+                type: GET_COMPANY,
+                payload:arrayOfCompany,
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
     }
 }
